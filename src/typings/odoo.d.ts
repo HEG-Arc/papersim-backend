1de3c253fe4276d92be7073cc8a908aadf4101bd
Boris Fritscher <boris@fritscher.ch>
2016-11-02 09:40:26 +0100

    add missing typings file for odoo lib

---
+++ src/typings/odoo.d.ts
@@ -0,0 +1,45 @@
+declare module 'odoo' {
+
+    interface OdooConfig {
+        host: string;
+        port?: number;
+        database: string;
+        username: string;
+        password: string;
+    }
+
+    interface SearchParams {
+        domain: any[][];
+    }
+
+    interface SearchReadParams {
+        domain: any[][];
+        limit: number;
+        offset?: number;
+        order?: string;
+        fields?: string[];
+        [x: string]: any
+    }
+
+    interface GetParams {
+        ids: number[];
+        fields?: string[];
+    }
+
+    export class Odoo {
+        constructor(config: OdooConfig)
+        connect(callback: (error: Error, result: any) => void):void;
+        search(model: string, params: SearchParams, callback: (error: Error, result: any) => void):void;
+        search_read(model: string, params: SearchReadParams, callback: (error: Error, result: any) => void):void;
+        get(model: string, params: GetParams, callback: (error: Error, result: any) => void):void;
+        browse_by_id(model: string, params: SearchReadParams, callback: (error: Error, result: any) => void):void;
+        create(model: string, params: any, callback: (error: Error, result: any) => void):void;
+        update(model: string, id: number, params: any, callback: (error: Error, result: any) => void):void;
+        delete(model: string, id: number, callback: (error: Error, result: any) => void):void;
+        rpc_call(endpoint: string, params: any, callback: (error: Error, result: any) => void):void;
+        public context:any;
+        public uid:number;
+        public sid:string;
+        public session_id:string;
+    }
+}

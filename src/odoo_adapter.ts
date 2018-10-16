d1f48076675e34e323612d03a0dff4e35ac1ddb3
Boris Fritscher <boris@fritscher.ch>
2018-10-16 16:57:27 +0200

    odoo 12 api changes

--- src/odoo_adapter.ts
+++ src/odoo_adapter.ts
@@ -1049,7 +1049,7 @@ export class OdooAdapter {
                 let res = await this.execute((resolve, reject) => {
                     this.odoo.rpc_call('/web/dataset/call_button', {
                         model: 'stock.picking',
-                        method: 'do_new_transfer',
+                        method: 'button_validate',
                         args: [[pickingId]]
                     }, this.createDefaultResponseHandler(resolve, reject));
                 });
@@ -1396,7 +1396,7 @@ export class OdooAdapter {
 			    product_qty: quantityProduced,
 			    product_tracking: 'none',
 			    lot_id: false,
-			    consume_line_ids: []
+			    produce_line_ids: []
             }, this.createDefaultResponseHandler(resolve, reject));
         });
         delete this.odoo.context.active_model;
